0f600cb19ab88d19227f329e038f4a52e34f58ae
gstimm <contato.gabrieltimm@gmail.com>
2021-08-31 15:13:34 -0300

    feat: Home Page finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import { GetStaticProps } from 'next';
-
-import { getPrismicClient } from '../services/prismic';
-
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import Head from 'next/head';
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import { useState } from 'react';
+import Header from '../components/Header';
 import commonStyles from '../styles/common.module.scss';
+import { getPrismicClient } from '../services/prismic';
 import styles from './home.module.scss';
 
 interface Post {
@@ -24,13 +30,113 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const { next_page, results } = postsPagination;
+  const [posts, setPosts] = useState<Post[]>(results);
+  const [nextPage, setNextPage] = useState<string>(next_page);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const loadPosts = () => {
+    if (nextPage) {
+      fetch(nextPage)
+        .then(response => response.json())
+        .then(data => {
+          const newPosts = data.results.map(post => ({
+            uid: post.uid,
+            first_publication_date: post.first_publication_date,
+            data: {
+              title: post.data.title,
+              subtitle: post.data.subtitle,
+              author: post.data.author,
+            },
+          }));
 
-//   // TODO
-// };
+          setNextPage(data.next_page);
+          setPosts([...posts, ...newPosts]);
+        })
+        .catch(() => {
+          alert('Falha ao carregar novos posts.');
+        });
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Home | SpaceTraveling</title>
+      </Head>
+      <Header />
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <Link key={post.uid} href={`/post/${post.uid}`}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <div>
+                  <FiCalendar size={20} />
+                  <time>
+                    {format(
+                      new Date(post.first_publication_date),
+                      'dd MMM yyyy',
+                      {
+                        locale: ptBR,
+                      }
+                    )}
+                  </time>
+                  <FiUser size={20} />
+                  <span>{post.data.author}</span>
+                </div>
+              </a>
+            </Link>
+          ))}
+          {nextPage && (
+            <button type="button" onClick={loadPosts}>
+              Carregar mais posts
+            </button>
+          )}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: [
+        'post.title',
+        'post.subtitle',
+        'post.author',
+        'post.banner',
+        'post.content',
+      ],
+      pageSize: 2,
+    }
+  );
+
+  const { next_page } = postsResponse;
+
+  const posts = postsResponse.results.map(post => ({
+    uid: post.uid,
+    first_publication_date: post.first_publication_date,
+    data: {
+      title: post.data.title,
+      subtitle: post.data.subtitle,
+      author: post.data.author,
+    },
+  }));
+
+  return {
+    props: {
+      postsPagination: {
+        next_page,
+        results: posts,
+      },
+    },
+    revalidate: 60 * 60, // 1 Hour
+  };
+};
